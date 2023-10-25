2765edfe7bc98543f730c7b90076db079bc48d4b
lehuubao1810 <lehuubao2909@gmail.com>
2023-10-25 22:47:23 +0700

    fix file name css

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../assets/css/Register.css";
+import "../assets/css/register.css";
 
 export default function Register() {
   const [username, setUsername] = useState("");
