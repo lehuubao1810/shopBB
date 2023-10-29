457dd80ec22ac3a1e584f8d0efe1abe085da82f8
lehuubao1810 <lehuubao2909@gmail.com>
2023-10-29 14:12:37 +0700

    responsive

--- client/src/pages/ResponseCheckout.jsx
+++ client/src/pages/ResponseCheckout.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+import "../assets/css/Response.css";
+
 export default function ResponseCheckout() {
   const isCheckout = false;
   const isResponse = false;
@@ -12,30 +14,36 @@ export default function ResponseCheckout() {
   return (
     <div className="responseCheckoutPage">
       <Header />
-      {isCheckout ? (
-        isResponse ? (
-          <h1>
-            Đơn hàng của bạn đã được đặt thành công. Chúng tôi sẽ gửi hóa đơn qua email cho bạn. 
-            <br />
-            Cảm ơn bạn đã mua hàng tại shop!
-          </h1>
+      <div className="responseCheckoutPage__content">
+        {isCheckout ? (
+          isResponse ? (
+            <h1>
+              Đơn hàng của bạn đã được đặt thành công. Chúng tôi sẽ gửi hóa đơn
+              qua email cho bạn.
+              <br />
+              Cảm ơn bạn đã mua hàng tại shop!
+            </h1>
+          ) : (
+            <h1>
+              Xin lỗi, đơn hàng của bạn không thể được đặt thành công. Vui lòng
+              thử lại sau.
+              <br />
+              Hoặc liên hệ với chúng tôi qua email:{" "}
+              <a href="mailto:bb@cskh.com">bb@cskh.com</a>
+            </h1>
+          )
         ) : (
-          <h1>
-            Xin lỗi, đơn hàng của bạn không thể được đặt thành công. Vui lòng thử lại sau. 
-            <br />
-            Hoặc liên hệ với chúng tôi qua email: <a href="mailto:bb@cskh.com">bb@cskh.com</a>
-          </h1>
-        )
-      ) : (
-        <div className="checkOutPage__container__empty">
-          <img
-            src="https://shopfront-cdn.tekoapis.com/static/empty_cart.png"
-            alt="empty cart"
-          />
-          <p>Không có đơn hàng nào được thực hiện</p>
-          <button onClick={() => navigate("/")}>Quay lại mua sắm</button>
-        </div>
-      )}
+          <div className="checkOutPage__container__empty">
+            <img
+              src="https://shopfront-cdn.tekoapis.com/static/empty_cart.png"
+              alt="empty cart"
+            />
+            <p>Không có đơn hàng nào được thực hiện</p>
+            <button onClick={() => navigate("/")}>Quay lại mua sắm</button>
+          </div>
+        )}
+      </div>
+
       <Footer />
     </div>
   );
