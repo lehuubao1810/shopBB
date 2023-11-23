492e7b462670e0628806fe85b12e265068f42032
lehuubao1810 <lehuubao2909@gmail.com>
2023-11-23 15:24:08 +0700

    Update register page styles

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 import { useAuth } from "../context/AuthContext";
 
 import "../assets/css/register.css";
@@ -8,11 +11,11 @@ import "../assets/css/register.css";
 export default function Register() {
   const { register } = useAuth();
 
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [usernameError, setUsernameError] = useState("");
+  const [nameError, setNameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
   const [emailError, setEmailError] = useState("");
@@ -20,111 +23,126 @@ export default function Register() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.title = "Đăng nhập | Shop BB";
+    document.title = "Đăng ký | Shop BB";
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // validate inputs
-    if (username === "") {
-      setUsernameError("Please enter a username");
+    if (name === "") {
+      setNameError("Vui lòng nhập họ và tên");
+      return;
     } else {
-      setUsernameError("");
+      setNameError("");
     }
     if (password === "") {
-      setPasswordError("Please enter a password");
+      setPasswordError("Vui lòng nhập mật khẩu");
+      return;
     } else if (password.length < 6) {
-      setPasswordError("Password must be at least 6 characters long");
+      setPasswordError("Mật khẩu phải có ít nhất 6 ký tự");
+      return;
     } else if (password.length > 30) {
-      setPasswordError("Password must be less than 30 characters long");
+      setPasswordError("Mật khẩu không được quá 30 ký tự");
+      return;
     } else {
       setPasswordError("");
     }
     if (confirmPassword === "") {
-      setConfirmPasswordError("Please confirm your password");
+      setConfirmPasswordError("Vui lòng nhập lại mật khẩu");
+      return;
     } else if (password !== confirmPassword) {
-      setConfirmPasswordError("Passwords do not match");
+      setConfirmPasswordError("Mật khẩu không khớp");
+      return;
     } else {
       setConfirmPasswordError("");
     }
     if (email === "") {
-      setEmailError("Please enter an email");
+      setEmailError("Vui lòng nhập email");
+      return;
     } else if (!/\S+@\S+\.\S+/.test(email)) {
-      setEmailError("Please enter a valid email address");
+      setEmailError("Email không hợp lệ");
+      return;
     } else {
       setEmailError("");
     }
-    // submit form if no errors
-    if (
-      usernameError === "" &&
-      passwordError === "" &&
-      confirmPasswordError === "" &&
-      emailError === ""
-    ) {
-      register(username, email, password);
-      navigate("/");
-    }
+    register(name, email, password);
+    navigate("/");
   };
 
   return (
     <div className="registerPage">
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {emailError && <span className="error">{emailError}</span>}
+      <Header />
+      <div className="registerPage__content">
+        <div className="registerPage__content__box">
+          <h2 className="registerPage__content__box__title">Đăng ký</h2>
+          <form onSubmit={handleSubmit} className="registerPage__content__box__form">
+            <div>
+              <label>
+                <p>Email</p>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              {emailError && <span className="form__error">{emailError}</span>}
+            </div>
+            <div>
+              <label>
+                <p>Họ và tên</p>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
+              {nameError && <span className="form__error">{nameError}</span>}
+            </div>
+            <div>
+              <label>
+                <p>Mật khẩu</p>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              {passwordError && <span className="form__error">{passwordError}</span>}
+            </div>
+            <div>
+              <label>
+                <p>Nhập lại mật khẩu</p>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </label>
+              {confirmPasswordError && (
+                <span className="form__error">{confirmPasswordError}</span>
+              )}
+            </div>
+            <button type="submit">Đăng ký</button>
+          </form>
+          <div className="registerPage__content__box__login">
+            <p>
+              Bạn đã có tài khoản ?{" "}
+              <span
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Đăng nhập
+              </span>
+            </p>
+          </div>
         </div>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          {usernameError && <span className="error">{usernameError}</span>}
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {passwordError && <span className="error">{passwordError}</span>}
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          {confirmPasswordError && (
-            <span className="error">{confirmPasswordError}</span>
-          )}
-        </div>
-
-        <button type="submit">Register</button>
-      </form>
-      <p>
-        Already have an account?{" "}
-        <button
-          onClick={() => {
-            navigate("/login");
-          }}
-        >
-          Login
-        </button>
-      </p>
+      </div>
+      <Footer />
     </div>
   );
 }
