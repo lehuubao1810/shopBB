4e198c85f598c96af189bf2401c6c069d1df8fc7
lehuubao1810 <lehuubao2909@gmail.com>
2023-11-28 18:08:25 +0700

    Update DashboardPage CSS import

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -12,7 +12,7 @@ import formatPrice from "../../utils/formatPrice";
 
 import { host } from "../../context/host";
 
-import "../../assets/css/admin/DashboardPage.css";
+import "../../assets/css/admin/CreateProduct.css";
 
 export default function Dashboard() {
   const [orders, setOrders] = useState([]);
