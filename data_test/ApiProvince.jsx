036768d4c62df33dd1dd3b211a6a5714f205b0b1
lehuubao1810 <lehuubao2909@gmail.com>
2023-11-30 14:01:37 +0700

    Add API endpoints for provinces, districts, and wards

--- data_test/ApiProvince.jsx
+++ data_test/ApiProvince.jsx
@@ -1,5 +1,3 @@
-
-
 // const apiProvince = https://provinces.open-api.vn/api/
 // const apiDistrict = https://provinces.open-api.vn/api/p/${provinceId}?depth=2
 // const apiWard = https://provinces.open-api.vn/api/d/${districtId}?depth=2
