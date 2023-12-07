440110745f9319744af904b4494b6e33cb45caf2
lehuubao1810 <lehuubao2909@gmail.com>
2023-12-07 13:50:11 +0700

    Update host URL

--- client/src/context/host.jsx
+++ client/src/context/host.jsx
@@ -1,5 +1,5 @@
 export const host = {
     // dev: "http://localhost:5000",
-    // dev: "https://shopbb.onrender.com"
-    dev: "http://3.0.56.176:5050"
+    dev: "https://shopbb.onrender.com"
+    // dev: "http://3.0.56.176:5050"
 }
