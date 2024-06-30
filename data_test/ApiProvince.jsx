a54197dcb5c092ffe2b3f72cfcd7a1645f1006b1
lehuubao1810 <lehuubao2909@gmail.com>
2024-06-30 18:14:53 +0700

    chore: Remove folder fake data

--- data_test/ApiProvince.jsx
+++
@@ -1,4 +0,0 @@
-// const apiProvince = https://provinces.open-api.vn/api/
-// const apiDistrict = https://provinces.open-api.vn/api/p/${provinceId}?depth=2
-// const apiWard = https://provinces.open-api.vn/api/d/${districtId}?depth=2
-
